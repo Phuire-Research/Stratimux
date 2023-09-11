1e4782e91cee04035c6ee3ee411dffb394585a10
REllEK-IO <micah.t.keller@gmail.com>
2023-09-11 12:11:06 -0700

    Development Notes

---
+++ src/concepts/ownership/strategies/ownershipHandleAddConcept.strategy.ts
@@ -0,0 +1,4 @@
+// PUNT, special case required for adding Concepts if Ownership is present
+//  As ordinarily we would allow for remove Concepts to have their Actions Expire in Ownership
+//  But if added back on within the time frame of agreements, these would have to be cleared.
+//  But likewise depending on the implementation, could be the intended behavior to save on memory.
