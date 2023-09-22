4dc9b907ba6fa0cdffa22f5fa868394150434c9e
REllEK-IO <micah.t.keller@gmail.com>
2023-09-22 15:05:33 -0700

    Additional Work

--- src/test/ownership.test.ts
+++ src/test/ownership.test.ts
@@ -5,12 +5,11 @@ import { OwnershipState, createOwnershipConcept, ownershipName } from '../concep
 import { AxiumState } from '../concepts/axium/axium.concept';
 import { setOwnerShipModeTopic, setOwnershipModeStrategy } from '../concepts/ownership/strategies/setOwnerShipMode.strategy';
 
-// REFACTOR to have two Axiums interact with One Another
-// Going to Punt for now. Moving back towards creating the UI
-// As this system is Reserved for a Complex Set Up and the Interaction
-//  And the Interaction with the File System by default is that Set Up
-// When this becomes important again. The requirement for a FailureNode to be that of Null
-// Kicks in. As a Block of a Key is a Failure by Default
+// REFACTOR
+// Create a Test Ownership Principle
+// And a Dummy set of Strategies that utilize default Method and Reducer
+// Refine as you Go
+
 test('Ownership Test', (done) => {
   const axium = createAxium([createOwnershipConcept()], true, true);
   const sub = axium.subscribe((concepts: Concept[]) => {
@@ -24,17 +23,9 @@ test('Ownership Test', (done) => {
   });
 });
 
-// Deno.test( 'Axium Selector Test', async () => {
-//   const counter = counterConcept;
-//   const counterState = counterConcept.state as Counter;
-//   counterState.count = 10;
-//   const axium = await createAxium([counter]);
-//   const sub = axium.subscribe((concepts: Concept[]) => {
-//     const state = selectState<Counter>(concepts, counter.key);
-//     console.log(`Count: ${state.count}`);
-//     assertEquals(state.count, 10);
-//   });
-// },
-//   // sanitizeResources: false,
-//   // sanitizeOps: false,
-// );
+// PUNT Additional Test: Have two Axiums interact with One Another
+// Going to Punt for now. Moving back towards creating the UI
+// As this system is Reserved for a Complex Set Up and the Interaction
+//  And the Interaction with the File System by default is that Set Up
+// When this becomes important again. The requirement for a FailureNode to be that of Null
+// Kicks in. As a Block of a Key is a Failure by Default
