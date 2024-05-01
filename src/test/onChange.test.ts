738af78d3df9d1b43e068ce2edcce3d0d0445788
REllEK-IO <micah.t.keller@gmail.com>
2024-05-01 09:59:19 -0700

    onChange test

---
+++ src/test/onChange.test.ts
@@ -0,0 +1,39 @@
+/*<$
+For the asynchronous graph programming framework Stratimux, generate a test that ensures that ActionStrategies are working as intended.
+$>*/
+/*<#*/
+import { createAxium } from '../model/axium';
+import { strategyBegin } from '../model/actionStrategy';
+import { selectState } from '../model/selector';
+import { CounterState, createCounterConcept, countingStrategy, counterName } from '../concepts/counter/counter.concept';
+import { AxiumState } from '../concepts/axium/axium.concept';
+import { countingTopic } from '../concepts/counter/strategies/counting.strategy';
+import { createStage } from '../model/stagePlanner';
+import { counterSelectCount } from '../concepts/counter/counter.selector';
+import { axiumSelectLastStrategy } from '../concepts/axium/axium.selector';
+
+test('Axium Counting Strategy Test', (done) => {
+  const axium = createAxium('axiumStrategyTest', [createCounterConcept()], true, true);
+  const plan = axium.plan('Counting Strategy Stage',
+    [
+      createStage((_, dispatch) => {
+        dispatch(strategyBegin(countingStrategy()), {
+          iterateStage: true
+        });
+      }),
+      createStage((concepts, _, changes) => {
+        console.log(changes);
+        const axiumState = concepts[0].state as AxiumState;
+        if (axiumState.lastStrategy === countingTopic) {
+          const counter = selectState<CounterState>(concepts, counterName);
+          expect(counter?.count).toBe(1);
+          setTimeout(() => {done();}, 500);
+          plan.conclude();
+          axium.close();
+        }
+      }, {
+        selectors: [counterSelectCount, axiumSelectLastStrategy]
+      })
+    ]);
+});
+/*#>*/
