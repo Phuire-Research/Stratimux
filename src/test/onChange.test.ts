a8583919a42649513744112331e08fec897f67a6
REllEK-IO <micah.t.keller@gmail.com>
2024-05-01 14:42:55 -0700

    v0.1.56

--- src/test/onChange.test.ts
+++ src/test/onChange.test.ts
@@ -5,35 +5,52 @@ $>*/
 /*<#*/
 import { createAxium } from '../model/axium';
 import { strategyBegin } from '../model/actionStrategy';
-import { selectState } from '../model/selector';
+import { selectSlice, selectState } from '../model/selector';
 import { CounterState, createCounterConcept, countingStrategy, counterName } from '../concepts/counter/counter.concept';
 import { AxiumState } from '../concepts/axium/axium.concept';
 import { countingTopic } from '../concepts/counter/strategies/counting.strategy';
 import { createStage } from '../model/stagePlanner';
 import { counterSelectCount } from '../concepts/counter/counter.selector';
 import { axiumSelectLastStrategy } from '../concepts/axium/axium.selector';
+import { axiumKick } from '../concepts/axium/qualities/kick.quality';
+import { initializeTopic } from '../concepts/axium/strategies/initialization.strategy';
+import { Concepts } from '../model/concept';
 
 test('Axium Counting Strategy Test', (done) => {
+  const selectorRouter = {
+    [axiumSelectLastStrategy.keys]: (concepts: Concepts) =>
+      console.log('CHECK: ', selectSlice(concepts, axiumSelectLastStrategy))
+  };
   const axium = createAxium('axiumStrategyTest', [createCounterConcept()], true, true);
   const plan = axium.plan('Counting Strategy Stage',
     [
-      createStage((_, dispatch) => {
-        dispatch(strategyBegin(countingStrategy()), {
-          iterateStage: true
+      createStage((concepts, dispatch) => {
+        if (selectSlice(concepts, axiumSelectLastStrategy) === initializeTopic) {
+          dispatch(strategyBegin(countingStrategy()), {
+            iterateStage: true
+          });
+        }
+      }, {selectors: [axiumSelectLastStrategy]}),
+      createStage((concepts, dispatch, changes) => {
+        console.log('Check Changes: ',  changes);
+        changes?.forEach(keyed => {
+          selectorRouter[keyed.keys] ? selectorRouter[keyed.keys](concepts) : null;
         });
-      }),
-      createStage((concepts, _, changes) => {
-        console.log(changes);
         const axiumState = concepts[0].state as AxiumState;
+        const counter = selectState<CounterState>(concepts, counterName);
         if (axiumState.lastStrategy === countingTopic) {
-          const counter = selectState<CounterState>(concepts, counterName);
           expect(counter?.count).toBe(1);
           setTimeout(() => {done();}, 500);
           plan.conclude();
           axium.close();
+        } else {
+          dispatch(axiumKick(), {
+            newSelectors: [counterSelectCount, axiumSelectLastStrategy],
+            throttle: 0
+          });
         }
       }, {
-        selectors: [counterSelectCount, axiumSelectLastStrategy]
+        selectors: [counterSelectCount]
       })
     ]);
 });
